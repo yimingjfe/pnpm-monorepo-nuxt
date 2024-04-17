82af371209d4a8e8c291fd624f7ab958f2e5261d
wangyiming <wangyiming.fe@bytedance.com>
2024-04-17 21:22:12 +0800

    feat: add execa

--- packages/nuxt-app/server/api/hello.ts
+++ packages/nuxt-app/server/api/hello.ts
@@ -1,6 +1,8 @@
+import { execa } from 'execa';
 import { addWrap } from 'utils'
 
 export default defineEventHandler(async() => {
+  await execa('echo', ['hello execa']);
   return {
     hello: addWrap(2, 3)
   }
